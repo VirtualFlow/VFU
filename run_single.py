cc03dd7d8b17d0f8bdf56c70d55c2eb2b181302f
akshat998 <akshat998@gmail.com>
2022-11-16 15:46:57 -0700

    rdock correction added

--- run_single.py
+++ run_single.py
@@ -18,7 +18,7 @@ command = []
 # Parameters:  
 is_selfies     = False 
 is_peptide     = False
-program_choice = 'vina' # smina/qvina/qvina-w/vina/vina_carb/vina_xb/gwovina/PLANTS/autodock_gpu/autodock_cpu/EquiBind/rDock/gnina/ledock/idock
+program_choice = 'idock' # smina/qvina/qvina-w/vina/vina_carb/vina_xb/gwovina/PLANTS/autodock_gpu/autodock_cpu/EquiBind/rDock/gnina/ledock/idock
                              # /autodock_vina/adfr/AutodockVina_1.2/AutodockZN/flexx/MM-GBSA/MCDock/LigandFit/GalaxyDock3/dock6/FRED/iGemDock/gold
                              # glide/rosetta-ligand/M-Dock/SEED/nnscore2/rf-score
                            
@@ -194,7 +194,7 @@ for lig_ in lig_locations:
         cmd = cmd + ['--log', './outputs/log_{}.txt'.format(lig_.split('.')[0])]
 
     # Run the command: 
-        
+    print('idock: ', cmd)
     raise Exception('T')
                 
     command_run = subprocess.run(cmd, capture_output=True)
